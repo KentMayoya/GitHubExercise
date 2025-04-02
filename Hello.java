f524c2e22b0de8f8b3d058bcd5a7947fc8f133be
Alan Talavera-Cordova <126989249+YoloMcFroyo@users.noreply.github.com>
2025-04-02 15:13:01 -0700

    Create new loop

--- Hello.java
+++ Hello.java
@@ -3,5 +3,9 @@ class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        for(int i = 0; i < 10; i++){
+            System.out.println("Kent likes boba");
+        }
     }
 }
