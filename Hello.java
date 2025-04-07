b9304e82ee3793176688ee20e675a06c72e0b9e0
Alan Talavera-Cordova <126989249+YoloMcFroyo@users.noreply.github.com>
2025-04-07 13:27:01 -0700

    Slob

--- Hello.java
+++ Hello.java
@@ -2,6 +2,8 @@
 class Hello {
 
     public static void main(String[] args) {
+        System.out.println("Droop Sloop");
+
         System.out.println("Hello world!");
 
         for(int i = 0; i < 10; i++){
